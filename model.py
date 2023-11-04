f96d75d4d9633a786b6895598a7ab9e37600bbcf
Erik <kontakt@erikkiessig.de>
2023-11-04 22:36:17 +0100

    Add mandatory datamodel

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from . import db
 import flask_login
 
+
 class FollowingAssociation(db.Model):
     follower_id = db.Column(
         db.Integer, db.ForeignKey("user.id"), primary_key=True, nullable=False
@@ -9,11 +10,14 @@ class FollowingAssociation(db.Model):
         db.Integer, db.ForeignKey("user.id"), primary_key=True, nullable=False
     )
 
+
 class User(flask_login.UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     name = db.Column(db.String(64), nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    recipes = db.relationship("Recipe", back_populates="user")
+    photos = db.relationship("Photo", back_populates="user")
     messages = db.relationship("Message", back_populates="user")
     following = db.relationship(
         "User",
@@ -29,6 +33,64 @@ class User(flask_login.UserMixin, db.Model):
         secondaryjoin=FollowingAssociation.follower_id == id,
         back_populates="following",
     )
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(512), nullable=False)
+    persons = db.Column(db.Integer, nullable=False)
+    estimated_time = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", back_populates="recipe")
+    q_ingredients = db.relationship("Q_ingredient", back_populates="recipe")
+    photos = db.relationship("Photo", back_populates="recipe")
+    steps = db.relationship("Step", back_populates="recipe")
+    rating = db.relationship("Rating", back_populates="recipe")
+
+
+class Q_ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    unit = db.Column(db.String(32), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    recipe = db.relationship("Recipe", back_populates="ingredient")
+    ingredient_id = db.Column(
+        db.Integer, db.ForeignKey("ingredient.id"), nullable=False
+    )
+    ingredient = db.relationship("Ingredient", back_populates="q_ingredient")
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    q_ingredients = db.relationship("Q_ingredient", back_populates="ingredient")
+
+
+class Step(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(512), nullable=False)
+    order = db.Column(db.Integer, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    recipe = db.relationship("Recipe", back_populates="step")
+
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer, nullable=False)  # 0 => downvote, 1 => upvote
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    recipe = db.relationship("Recipe", back_populates="rating")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", back_populates="rating")
+
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_extension = db.Column(db.String(8), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
+    recipe = db.relationship("Recipe", back_populates="photo")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", back_populates="photo")
 
 
 class Message(db.Model):
